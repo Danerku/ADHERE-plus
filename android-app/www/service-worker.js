ad70f5371fbeded06e6b51cd4566711532028ba9
Danerku <daniel.asfaw05@gmail.com>
2026-07-12 17:54:15 -0500

    Mirror v5.0 into the Android bundle

--- android-app/www/service-worker.js
+++ android-app/www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='adhere-v50';
+const CACHE='adhere-v51';
 const SHELL=['./','./index.html','./app.js','./styles.css','./config.js','./manifest.webmanifest',
   './model/score.js','./model/bayes_tracker.js','./model/rules_engine.js','./model/charts.js','./model/ethiopian.js',
   './model/risk_model.json','./model/newborn_model.json','./model/mch_rules.json'];
